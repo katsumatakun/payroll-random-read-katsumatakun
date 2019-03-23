0c27f5703818afa7f90f3de152437ad9c09fb849
MasahiroKiji <mkiji@coe.edu>
2019-03-22 20:08:27 -0500

    Add a feature of magic number

--- payget.c
+++ payget.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "emp_rec.h"
 #include "print_calculated.h"
 
@@ -11,21 +12,33 @@ int main(int argc, char* argv[]) {
       printf("Input file not open\n");
       return -1;
     }
+
+    char* magic_num = "PAYOUT@#";
+    char* first_eight = (char*) malloc(8);
+    fread(first_eight, 8, 1, fpr);
+    
+    if(strcmp(magic_num, first_eight)){
+      printf("Invaild File\n");
+      return -1;
+    }
+
     empPtr q;
     pdataPtr p;
     char *endp;
     int arg_num = 2;
+    printf("Last name, First name  bi-weekly pay  federal tax  state tax  insurance     net pay \n");
     while(arg_num != argc){
     int record_num = strtol(argv[arg_num], &endp, 10);
-    printf("%d\n", record_num);
-    if (*endp == '\0'){
+    if (*endp != '\0')
+      printf("Input value must be integer\n");
+    else
+    {
       if((q = (empPtr) malloc(sizeof(struct emp))) == NULL || (p = (pdataPtr) malloc(sizeof(struct paydata)))== NULL){
         printf("No more memory space available \n");
         return -1;
       }
 
-      printf("Last name, First name  bi-weekly pay  federal tax  state tax  insurance     net pay \n");
-      fseek(fpr, (sizeof(struct emp) + sizeof(struct paydata))*(abs(record_num)-1), SEEK_SET);
+      fseek(fpr, (sizeof(struct emp) + sizeof(struct paydata))*(abs(record_num)-1)+8, SEEK_SET);
       if(record_num <= 0 && fpr != NULL){
         fseek(fpr, (sizeof(struct emp) + sizeof(struct paydata))*(record_num), SEEK_END);
       }
@@ -36,9 +49,6 @@ int main(int argc, char* argv[]) {
       else{
         printf("There is no record for record%d\n", record_num);
       }
-    }
-    else{
-      printf("Input value must be integer\n");
     }
     arg_num++;
   }
