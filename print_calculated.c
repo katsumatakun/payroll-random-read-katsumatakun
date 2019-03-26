55f3206026d9b730818ce2317bc2a18df408dc3a
MasahiroKiji <mkiji@coe.edu>
2019-03-25 19:05:46 -0500

    Update print format and Makefile

--- print_calculated.c
+++ print_calculated.c
@@ -9,6 +9,13 @@
 
 */
 void print_emp_data(empPtr q, pdataPtr p){
-  printf("%-10s %-14s %6.2f %13.2f %10.2f %11.2f %12.2f \n",q->last_name, q->first_name,
+  printf("=========================================================================\n");
+  printf("Last name, First name address\n");
+  printf("%-10s %-10s %-10s %-10s %-3s %-10s  \n",q->last_name, q->first_name, q->addr,
+  q->city, q->state, q->zipcode);
+  printf("==========================================================================\n");
+  printf("bi-weekly pay  federal tax  state tax  insurance     net pay \n");
+  printf("%7.2f %14.2f %12.2f %11.2f %12.2f \n",
   p->biWeeklySalary, p->fedTaxDeducted, p->stateTaxDeducted, p->medicalDeducted, p->netPay);
+  printf("==========================================================================\n\n");
 }
